19c42d826bb6049763f7936a265eab65f653356a
jonathan-tapia <jonathan@visioncourse.com>
2018-06-17 23:50:46 -0700

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,7 +1,11 @@
-from picamera import PiCamera
-from time import sleep
+import picamera
+import time
 
 camera = PiCamera()
+camera.resolution = (1024, 768)
+
+def closeCamera():
+	camera.close()
 
 def startPreview():
 	camera.start_preview()
@@ -9,6 +13,10 @@ def startPreview():
 def stopPreview():
 	camera.stop_preview()
 
+def takePicture():
+	camera.capture('foo.jpg')
+
 startPreview()
-sleep(10)
+sleep(5)
+takePicture()
 stopPreview()
